3625c948b18a8af7efd4cf5ca6067823e8b9e1df
XiaooooooBawang <1206286599@qq.com>
2022-09-02 16:43:53 +0800

    add elasticsearch

--- hotel-demo/src/test/java/cn/xbw/hotel/HotelDocumentTest.java
+++ hotel-demo/src/test/java/cn/xbw/hotel/HotelDocumentTest.java
@@ -4,6 +4,7 @@ import cn.xbw.hotel.pojo.Hotel;
 import cn.xbw.hotel.pojo.HotelDoc;
 import cn.xbw.hotel.service.IHotelService;
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.bulk.BulkRequest;
@@ -11,6 +12,8 @@ import org.elasticsearch.action.delete.DeleteRequest;
 import org.elasticsearch.action.get.GetRequest;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
@@ -18,6 +21,14 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
+import org.elasticsearch.search.sort.SortOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -26,11 +37,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 import static cn.xbw.hotel.constants.HotelConstants.MAPPING_TEMPLATE;
 
 @SpringBootTest
-public class HotelDocumentTest {
+class HotelDocumentTest {
     private RestHighLevelClient client;
 
     @Resource
@@ -43,7 +55,8 @@ public class HotelDocumentTest {
     @BeforeEach
     void setUp() {
         this.client = new RestHighLevelClient(RestClient.builder(
-                HttpHost.create("http://192.168.0.100:9200")
+                //HttpHost.create("http://192.168.0.100:9200")
+                HttpHost.create("http://172.20.10.4:9200")
         ));
     }
 
@@ -158,5 +171,164 @@ public class HotelDocumentTest {
         }
         // 3.发送请求
         client.bulk(request, RequestOptions.DEFAULT);
+    }
+
+    /**
+     * match_all
+     *
+     * @throws IOException
+     */
+    @Test
+    void tesMatchAll() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 3.发送请求,得到响应
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+
+    /**
+     * match 查询
+     * @throws IOException
+     */
+    @Test
+    void testMatch() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL，match是单字段查询，multimatch是多字段查询
+        request.source().query(QueryBuilders.matchQuery("all", "如家"));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 精确查询
+     * @throws IOException
+     */
+    @Test
+    void testQuery() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        request.source().query(
+                //term词条查询
+//                QueryBuilders.termQuery("city", "上海")
+                //range范围查询
+                QueryBuilders.rangeQuery("price").gt(100).lt(150)
+        );
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 布尔查询
+     *
+     * @throws IOException
+     */
+    @Test
+    void testBoolQuery() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        // 2.1 创建布尔查询器BoolQueryBuilder
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        // 2.2.添加term
+        boolQuery.must(QueryBuilders.termQuery("city", "上海"));
+        // 2.3.添加range
+        boolQuery.filter(QueryBuilders.rangeQuery("price").lte(250));
+
+        request.source().query(boolQuery);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 分页和排序查询
+     *
+     * @throws IOException
+     */
+    @Test
+    void testPageAndSort() throws IOException {
+        // 页码，每页大小
+        int page = 1, size = 5;
+
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        // 2.1.query
+        request.source().query(QueryBuilders.matchAllQuery());
+        // 2.2.排序 sort
+        request.source().sort("price", SortOrder.ASC);
+        // 2.3.分页 from、size
+        request.source().from((page - 1) * size).size(size);
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 关键字高亮
+     *
+     * @throws IOException
+     */
+    @Test
+    void testHighlight() throws IOException {
+        // 1.准备Request
+        SearchRequest request = new SearchRequest("hotel");
+        // 2.准备DSL
+        //query,高亮查询必须使用全文检索查询，并且要有搜索关键字，将来才可以对关键字高亮
+        request.source().query(QueryBuilders.matchQuery("all", "如家"));
+        // 2.2.高亮
+        request.source().highlighter(new HighlightBuilder().field("name").requireFieldMatch(false));
+        // 3.发送请求
+        SearchResponse response = client.search(request, RequestOptions.DEFAULT);
+        // 4.解析响应
+        handleResponse(response);
+    }
+
+    /**
+     * 解析响应,能处理高亮
+     *
+     * @param response 响应
+     */
+    private void handleResponse(SearchResponse response) {
+        // 4.解析响应
+        SearchHits searchHits = response.getHits();
+        // 4.1.获取总条数
+        long total = searchHits.getTotalHits().value;
+        System.out.println("共搜索到" + total + "条数据");
+        // 4.2.文档数组
+        SearchHit[] hits = searchHits.getHits();
+        // 4.3.遍历
+        for (SearchHit hit : hits) {
+            // 获取文档source
+            String json = hit.getSourceAsString();
+            // 反序列化
+            HotelDoc hotelDoc = JSON.parseObject(json, HotelDoc.class);
+            // 获取高亮结果
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (!CollectionUtils.isEmpty(highlightFields)) {
+                // 根据字段名获取高亮结果
+                HighlightField highlightField = highlightFields.get("name");
+                if (highlightField != null) {
+                    // 获取高亮值
+                    String name = highlightField.getFragments()[0].string();
+                    // 覆盖非高亮结果
+                    hotelDoc.setName(name);
+                }
+            }
+            System.out.println("hotelDoc = " + hotelDoc);
+        }
     }
 }
