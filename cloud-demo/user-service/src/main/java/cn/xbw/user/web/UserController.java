a27d7e482e9672590e2a2f2cc45cda468c434ebe
XiaooooooBawang <1206286599@qq.com>
2022-09-04 14:46:26 +0800

    fix bad request

--- cloud-demo/user-service/src/main/java/cn/xbw/user/web/UserController.java
+++ cloud-demo/user-service/src/main/java/cn/xbw/user/web/UserController.java
@@ -51,10 +51,13 @@ public class UserController {
      * 路径： /user/110
      *
      * @param id 用户id
+     * @param author 请求头中的author权限，因为传递的参数可能为空值（没有提交这个参数），在用feign调用（order调user）的时候并不会提交
+     *               这个author参数，API会抛异常，报400，bad request，所以RequestHeader 要加上 required = false，可以要求不一定提交
+     *
      * @return 用户
      */
     @GetMapping("/{id}")
-    public User queryById(@PathVariable("id") Long id, @RequestHeader("author") String author) {
+    public User queryById(@PathVariable("id") Long id, @RequestHeader(value = "author", required = false) String author) {
         System.out.println("author = " + author);  //验证一下gateway中的过滤器
         return userService.queryById(id);
     }
